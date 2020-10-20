597ee954d69062a177e91c90b4bde45d0807265b
nadineuwineza <nadineuwineza2017@gmail.com>
2020-10-20 14:11:39 +0200

    updating html

--- js/index.js
+++ js/index.js
@@ -1,83 +1,12 @@
-
-
-  // var sumTotal = 0;
-  // var toppingsArray= [];
-  // var toppingsNbr;
-  // var pizzaSize;
-  // var pizzaCrust;
-  // var pizzaNbr;
-  // function Order(number,size,crust,toppings,price){
-  //   this.number = number;
-  //   this.size=size;
-  //   this.crust=crust;
-  //   this.toppings=toppings;
-  //   this.price=price;
-  // }
-  // $(document).ready(function(){
-  //   $("#check-deliver").click(function () {
-  //     if ($(this).is(":checked")) {
-  //         $("#deliver-hide").show();
-  //         sumTotal= sumTotal+5;
-  //     } else {
-  //         $("#deliver-hide").hide();
-  //         sumTotal=sumTotal-5;
-  //     }
-  // });
-  //   $('#order-form').submit(function(event){
-  //     event.preventDefault();
-  //     pizzaSize= $('#pizza-size').val();
-  //     pizzaNbr= $("#pizza-number").val();
-  //     pizzaCrust= $("#crust").val();
-  //     var address= $('#street').val();
-  //     var pizzaToppings= [];
-     
-  //     $('div#toppings:checkbox:checked').each(function(i){
-  //       pizzaToppings[i] = $(this).val();
-  //       toppingsArray[i] = $(this).attr('name')
-  //     });
-  //     toppingsList = toppingsArray.join(',');
-  //     toppingsNbr= toppingsArray.length;
-  //     var orderPrice= priceCalc();
-  //     sumTotal = sumTotal + orderPrice;
-  //     if($('#check-deliver').is(":checked")){
-  //       alert("order will be delivered at " + Street);
-  //     }
-  //     var newOrder= new Order(pizzaNbr,pizzaSize,pizzaCrust,orderPrice)
-  //     $("ul#orders").append('<li><span>'+ newOrder.theOrder() +'</span></li>');
-  //     $("#total").text("total:" + sumTotal)
-  //     $("#deli-hide").hide();
-  //   });
-   
-  // });
-  // function priceCalc(){
-  //   var crustPrice,toppingsPrice;
-  //   var sizeChange;
-  //   var totalPrice;
-  //   if(pizzaSize=='Small'){
-  //     sizeChange=$2;
-  //   }else if(pizzaSize=='Medium'){
-  //     sizeChange=$4;
-  //   }else if(pizzaSize=='Large'){
-  //     sizeChange=6;
-  //   }
-  //   if (pizzaCrust=='Crispy'){
-  //     crustPrice=sizeChange*1.5;
-  //   }else if(pizzaCrust=='Stuffed'){
-  //     crustPrice=sizeChange * 2;
-  //   }else if(pizzaCrust=='Gluten-free'){
-  //     crustPrice=sizeChange*3;
-  //   }
-  //   toppingsPrice= toppingsNbr * sizeChange;
-  //   totalPrice= (crustPrice+toppingsPrice)*pizzaNbr;
-  //   return totalPrice;
-  // }
-  // order.prototype.theOrder = function (){
-  //   return this.number + " " + this.size + "pizzas, " +  this.crust + ",with " + this.toppings + " toppings.<br> Cost:"+this.price+"$";
-  // }
-  // function reload(){
-  //   location.reload();
-  // }
-  $(document).ready(function(){
+var sumPrice = 0;
+function Order(number,size,crust,toppings,price){
+  this.number = number;
+  this.size=size;
+  this.crust=crust;
+  this.toppings=toppings;
+  this.price=price;
+}
+$(document).ready(function(){
     $("#check-deliver").click(function(){
 if ($(this).is(":checked")){
 $("#deliver-hide").show()
@@ -85,37 +14,43 @@ $("#deliver-hide").show()
 else{
   $("#deliver-hide").hide()
 }
-
-    }); 
+    });
   });
-  $("#order-form").submit(function(){
+  $("#order-form").submit(function(event){
+    event.preventDefault();
 var pizzaSize=$("#pizza-size").val();
-var pizzCrust=$("#pizza-crust").val();
+var pizzaCrust=$("#pizza-crust").val();
 var pizzaToppings=[];
 var pizzaNbr=$("#pizza-number").val();
 $("div#toppings:checkbox:checked").each(function(i){
 pizzaToppings[i]=$(this).attr("name");
+});
+var price= myFunction(pizzaNbr,pizzaSize,pizzaCrust,pizzaToppings.length)
+sumPrice= sumPrice + price;
+var order = new Order(pizzaNbr,pizzaSize,pizzaCrust,pizzaToppings.length,price)
+$("ul#orders").append("<li>"+order.number +" "+order.size+" "+order.crust+ " pizzas "+order.price+"$</li>")
+$("#total").text(sumPrice);
+$("#order-form")[0].reset();
+alert("your order will be delivered to your location");
 
 });
-  });
   function myFunction(number,size,crust,toppings) {
-    if (pizzaCrust=='Crispy'){
+    var crustPrice,sizePrice,totalPrice,priceToppings,numberPrice;
+    if (crust=='Crispy'){
           crustPrice=2;
-        }else if(pizzaCrust=='Stuffed'){
+        }else if(crust=='Stuffed'){
           crustPrice=3;
-        }else if(pizzaCrust=='Gluten-free'){
+        }else if(crust=='Gluten-free'){
           crustPrice=5;
         }
-        if(pizzaSize=='Small'){
+        if(size=='Small'){
               sizePrice=2;
-            }else if(pizzaSize=='Medium'){
+            }else if(size=='Medium'){
               sizePrice=4;
-            }else if(pizzaSize=='Large'){
+            }else if(size=='Large'){
               sizePrice=6;
             }
-        priceToppings=2*pizzaNbr
-        totalPrice=(pizzaCrust+pizzaToppings)*pizzaSize*pizzaNbr
+        priceToppings=2*toppings;
+        totalPrice=(crustPrice+priceToppings)*sizePrice*number;
         return totalPrice;
       }
- 
-      
